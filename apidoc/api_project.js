d8b17d8cc8c084ceb0249a9a14abc590f9d911c8
rinkeshpatel22 <rinkeshpatel22@gmail.com>
2019-11-01 18:24:39 +0530

    apidoc added

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Sudoku Solver",
+  "version": "1.0.0",
+  "description": "Documentation for the Sudoku Solver server side application",
+  "title": "Sudoku Solver Server Side Apllication",
+  "url": "http://localhost:3000",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-11-01T10:21:50.782Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
